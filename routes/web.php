b4c8461f4c2c3ed8cffc622a2153b4cb395a5315
rintaro uchida <v795b3eed7qzaE05SE@softbank.ne.jp>
2021-04-27 11:25:23 +0900

    ルーティング生成

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,20 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    
+    Route::get('/review', 'PostController@create')->name('create');
+
+    Route::post('/store','PostController@store');
+
+    Route::get('/show_all','PostController@show_all')->name('show_all');
+
+    Route::get('/show_likes','PostController@show_likes')->name('show_likes');
+
+    Route::get('/search',function(){
+        return view('search');
+    })->name('search');
+    Route::post('/show_search','Postcontroller@show_search')->name('show_search');
+    Route::post('/ajaxlike', 'PostController@ajaxlike')->name('ajaxlike');
+});
